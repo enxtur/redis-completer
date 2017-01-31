5584faea6783bd19b10cde23c0d303d0bf5100ce
Enxtur Enxbat <enxtur@Enxturs-iMac.local>
2017-01-31 17:19:51 +0800

    sync

--- completer.js
+++ completer.js
@@ -1,4 +1,4 @@
-var r = require('redis').createClient();
+var redis = require('redis');
 var _ = require('underscore');
 var fs = require('fs');
 
@@ -8,32 +8,47 @@ var _appPrefix = '';
 var ZKEY_COMPL = 'compl';
 var ZKEY_DOCS_PREFIX = 'docs:';
 
-exports.applicationPrefix = applicationPrefix = function(prefix) {
+var r; // redis client
+
+// Try to connect to localhost by default to preserve backwards compatibility.
+r = redis.createClient();
+r.on("error", function (err) {
+  console.log("Redis error: " + err);
+  console.log('Redis-Completer unable to connect to redis on 127.0.0.1:6379.');
+  this.closing = true; // Don't retry.
+});
+
+// Initialization function that allows for a custom redis host and port.
+module.exports = function (redis_port, redis_host) {
+  r = redis.createClient(redis_port, redis_host);
+  return this;
+};
+
+module.exports.applicationPrefix = applicationPrefix = function (prefix) {
   // update key prefixes with user-specified application prefix
   _appPrefix = prefix;
   ZKEY_COMPL = prefix + ':' + 'compl';
   ZKEY_DOCS_PREFIX = prefix + ':' + 'docs:';
 };
 
-exports.deleteAll = deleteAll = function(cb) {
+module.exports.deleteAll = deleteAll = function (cb) {
   // clear all data
   r.zremrangebyrank(ZKEY_COMPL, 0, -1, cb);
 }
 
-exports.counter = 0;
+module.exports.counter = 0;
 
-exports.addCompletions = addCompletions = function (phrase, id, score, cb) {
+module.exports.addCompletions = addCompletions = function (phrase, id, score, cb) {
   // Add completions for originalText to the completions trie.
   // Store the original text, prefixed by the optional 'key'
 
   if (typeof score === 'function') {
-      cb = score;
-      score = null;
+    cb = score;
+    score = null;
   }
 
-  
   var text = phrase.trim().toLowerCase();
-  if (! text) {
+  if (!text) {
     return null, null;
   }
 
@@ -43,69 +58,69 @@ exports.addCompletions = addCompletions = function (phrase, id, score, cb) {
     phraseToStore = phrase;
   }
 
-  _.each(text.split(/\s+/), function(word) {
-    for (var end_index=1; end_index <= word.length; end_index++) {
+  _.each(text.split(/\s+/), function (word) {
+    for (var end_index = 1; end_index <= word.length; end_index++) {
       var prefix = word.slice(0, end_index);
-      exports.counter++;
+      module.exports.counter++;
       r.zadd(ZKEY_COMPL, 0, prefix, cb);
     }
-    exports.counter++;
-    r.zadd(ZKEY_COMPL, 0, word+'*', cb);
-    exports.counter++;
-    r.zadd(ZKEY_DOCS_PREFIX + word, score||0, phraseToStore, cb);
+    module.exports.counter++;
+    r.zadd(ZKEY_COMPL, 0, word + '*', cb);
+    module.exports.counter++;
+    r.zadd(ZKEY_DOCS_PREFIX + word, score || 0, phraseToStore, cb);
   });
 }
 
-exports.addFromFile = addFromFile = function(filename) {
+module.exports.addFromFile = addFromFile = function (filename) {
   // reads the whole file at once
   // no error-checking. just cross your fingers.
-  fs.readFile(filename, function(err, buf) {
+  fs.readFile(filename, function (err, buf) {
     if (err) {
       return console.log("ERROR: reading " + filename + ": " + err), null;
     }
-    _.each(buf.toString().split(/\n/), function(s) { addCompletions(s)});
+    _.each(buf.toString().split(/\n/), function (s) { addCompletions(s)});
   });
 }
 
-exports.getWordCompletions = getWordCompletions = function(word, count, callback) {
+module.exports.getWordCompletions = getWordCompletions = function (word, count, callback) {
   // get up to count completions for the given word
   // if prefix ends with '*', get the next exact completion
   var rangelen = 50;
- 
+
   var prefix = word.toLowerCase().trim();
-  var getExact = word[word.length-1] === '*'
+  var getExact = word[word.length - 1] === '*'
   var results = []
 
-  if (! prefix) {
+  if (!prefix) {
     return callback(null, results);
   }
 
-  r.zrank(ZKEY_COMPL, prefix, function(err, start) {
-    if (! start) {
+  r.zrank(ZKEY_COMPL, prefix, function (err, start) {
+    if (!start) {
       return callback(null, results);
     }
 
-    r.zrange(ZKEY_COMPL, start, start + rangelen - 1, function(err, entries) {
+    r.zrange(ZKEY_COMPL, start, start + rangelen - 1, function (err, entries) {
       while (results.length <= count) {
 
-        if (! entries || entries.length === 0) {
+        if (!entries || entries.length === 0) {
           break;
         }
-        
-        for (var i=0; i<entries.length; i++) {
+
+        for (var i = 0; i < entries.length; i++) {
           var entry = entries[i];
           var minlen = Math.min(entry.length, prefix.length);
 
           if (entry.slice(0, minlen) !== prefix.slice(0, minlen)) {
-            return callback(null,  results);
+            return callback(null, results);
           }
 
-          if (entry[entry.length-1] === '*' && results.length <= count) {
+          if (entry[entry.length - 1] === '*' && results.length <= count) {
             results.push(entry.slice(0, -1));
             if (getExact) {
               return callback(null, results);
             }
-          } 
+          }
         }
       }
       return callback(null, results);
@@ -113,7 +128,7 @@ exports.getWordCompletions = getWordCompletions = function(word, count, callback
   });
 }
 
-exports.getPhraseCompletions = getPhraseCompletions = function(phrase, count, callback) {
+module.exports.getPhraseCompletions = getPhraseCompletions = function (phrase, count, callback) {
 
   // when getting phrase completions, we should find a fuzzy match for the last
   // word, but treat the words before it as what the user intends.  So for
@@ -128,12 +143,12 @@ exports.getPhraseCompletions = getPhraseCompletions = function(phrase, count, ca
   var resultSet = {};
   var iter = 0;
 
-  _.each(prefixes, function(prefix) {
-    getWordCompletions(prefix, count, function(err, results) {
+  _.each(prefixes, function (prefix) {
+    getWordCompletions(prefix, count, function (err, results) {
       if (err) {
         callback(err, []);
       } else {
-        _.each(results, function(result) {
+        _.each(results, function (result) {
           // intending this to be a set, so we only
           // add each result once
           resultSet[result] = result;
@@ -143,7 +158,7 @@ exports.getPhraseCompletions = getPhraseCompletions = function(phrase, count, ca
 
       if (iter === prefixes.length) {
         // map set back to list
-        var resultList = _.map(resultSet, function(val, key) { return key; });
+        var resultList = _.map(resultSet, function (val, key) { return key; });
         callback(null, resultList);
       }
 
@@ -151,29 +166,29 @@ exports.getPhraseCompletions = getPhraseCompletions = function(phrase, count, ca
   });
 }
 
-exports.search = search = function(phrase, count, callback) {
+module.exports.search = search = function (phrase, count, callback) {
   // @callback with up to @count matches for @phrase
   var count = count || 10;
-  var callback = callback || function() {};
+  var callback = callback || function () {};
 
-  getPhraseCompletions(phrase, 10, function(err, completions) {
+  getPhraseCompletions(phrase, 10, function (err, completions) {
     if (err) {
       callback(err, null);
     } else {
-      var keys = _.map(completions, function(key) { 
-        return ZKEY_DOCS_PREFIX+key 
+      var keys = _.map(completions, function (key) {
+        return ZKEY_DOCS_PREFIX + key
       });
 
-      if (keys.length) { 
+      if (keys.length) {
         var results = {};
         var iter = 0;
 
         // accumulate docs and the scores for each key
-        
-        _.each(keys, function(key) {
+
+        _.each(keys, function (key) {
           r.zrevrangebyscore(key, 'inf', 0, 'withscores', function (err, docs) {
             // returns a list of [doc, score, doc, score ...]
-            iter ++;  
+            iter++;
             if (err) {
               return callback(err, {});
             } else {
@@ -194,7 +209,7 @@ exports.search = search = function(phrase, count, callback) {
               for (var key in results) {
                 ret.push(key);
               }
-              ret.sort(function(a,b) { return results[b] - results[a] });
+              ret.sort(function (a, b) { return results[b] - results[a] });
               return callback(null, ret);
             }
 
